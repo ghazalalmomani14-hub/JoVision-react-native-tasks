0e8ab315498668202b141e932b2ef8e3cbe88221
ghazalmomani20 <gmmomani20@cit.just.edu.jo>
2026-01-23 11:54:12 +0300

    Initial React Native project setup

---
+++ Tasks/Task16.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Task16 = () => {
+  return (
+    <View>
+      <Text>Task 16</Text>
+    </View>
+  );
+};
+
+export default Task16;
